115742869b8a65fab049dbf16aa438503086cc23
4AirtonLeal <52534519+4AirtonLeal@users.noreply.github.com>
2024-05-14 12:48:35 +0100

    Update CidadaoEndPoint.java

--- src/main/java/co/ao/BotaoDePanico/EndPoint/CidadaoEndPoint.java
+++ src/main/java/co/ao/BotaoDePanico/EndPoint/CidadaoEndPoint.java
@@ -64,7 +64,7 @@ public class CidadaoEndPoint {
         return new ResponseEntity<>(cidadaoDAO.save(cidadao), HttpStatus.OK);
     }
 
-    @PostMapping(path = "cidadao")
+    @PostMapping(path = "/cidadao")
     @ApiOperation(value = "Permite salvar um (1) novo cidadão na base de dados", response = TbCidadao[].class)
     public ResponseEntity<?> save(@Valid @RequestBody TbCidadao cidadao) {
         return new ResponseEntity<>(cidadaoDAO.save(cidadao), HttpStatus.CREATED);
